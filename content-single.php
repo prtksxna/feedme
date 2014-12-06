2f83c5a79ee37f5d3612bc517ffc7be9efc50e99
Prateek Saxena <prtksxna@gmail.com>
2014-12-06 17:31:51 +0530

    Remove the -t prefix in content files. Closes #4

--- content-single.php
+++ content-single.php
@@ -6,39 +6,39 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( has_category('post') ) {
-		get_template_part( 'content', 't-post' );
+		get_template_part( 'content', 'post' );
 	} ?>
 
 	<?php if ( has_category('chat') ) {
-		get_template_part( 'content', 't-chat' );
+		get_template_part( 'content', 'chat' );
 	} ?>
 
 	<?php if ( has_category('link') ) {
-		get_template_part( 'content', 't-link' );
+		get_template_part( 'content', 'link' );
 	} ?>
 
 	<?php if ( has_category('photo') ) {
-		get_template_part( 'content', 't-photo' );
+		get_template_part( 'content', 'photo' );
 	} ?>
 
 	<?php if ( has_category('status') ) {
-		get_template_part( 'content', 't-status' );
+		get_template_part( 'content', 'status' );
 	} ?>
 
 	<?php if ( has_category('quote') ) {
-		get_template_part( 'content', 't-quote' );
+		get_template_part( 'content', 'quote' );
 	} ?>
 
 	<?php if ( has_category('trip') ) {
-		get_template_part( 'content', 't-trip' );
+		get_template_part( 'content', 'trip' );
 	} ?>
 
 	<?php if ( has_category('thing') ) {
-		get_template_part( 'content', 't-thing' );
+		get_template_part( 'content', 'thing' );
 	} ?>
 
 
 	<?php if ( has_category('check-in') ) {
-		get_template_part( 'content', 't-checkin' );
+		get_template_part( 'content', 'checkin' );
 	} ?>
 </article><!-- #post-## -->
