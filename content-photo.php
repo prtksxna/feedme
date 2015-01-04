2ceb1c521e6c692471676b5bb7813db6407d007a
Prateek Saxena <prtksxna@gmail.com>
2015-01-04 20:13:54 +0530

    Make the picture clickable in "Photo" type post
    Closes #14

--- content-photo.php
+++ content-photo.php
@@ -1,5 +1,7 @@
 <div class="photo">
-	<img src="<?php echo get_field('image') ?>" alt="<?php echo the_title() ?>">
+	<a href="<?php echo the_permalink() ?>">
+		<img src="<?php echo get_field('image') ?>" alt="<?php echo the_title() ?>">
+	</a>
 	<h1><a href="<?php echo the_permalink() ?>"><?php echo the_title() ?></a></h1>
 	<footer>
 		<a href="<?php echo the_permalink() ?>">
