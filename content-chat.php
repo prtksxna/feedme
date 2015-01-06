dd2a902deef1c00e522bcbcec77f7915ebab83ab
Prateek Saxena <prtksxna@gmail.com>
2015-01-06 10:23:23 +0530

    chat: Use h-entry
    Also, use the <time> tag for the published date
    
    Bug: #36

--- content-chat.php
+++ content-chat.php
@@ -1,15 +1,17 @@
 <div class="chat">
 <header class="entry-header">
-	<h1><a href="<?php echo the_permalink() ?>"><?php echo the_title() ?></a></h1>
+	<h1><a href="<?php echo the_permalink() ?>" class="p-name u-url"><?php echo the_title() ?></a></h1>
 	<div class="entry-meta">
 		<a href="<?php echo the_permalink() ?>">
-			<?php the_date() ?> at
-			<?php the_time() ?>
+			<time datetime="<?php the_time( 'Y-m-d H:i' ) ?>" class="dt-published">
+				<?php the_date() ?> at
+				<?php the_time() ?>
+			</time>
 		</a>
 	</div><!-- .entry-meta -->
 </header><!-- .entry-header -->
 
-<div class="entry-content">
+<div class="entry-content e-content">
 	<?php the_content(); ?>
 </div>
 
