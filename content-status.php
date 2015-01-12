b8a2c0e3c56dab1ea83ded6cfea1152c9dbcd01f
Prateek Saxena <prtksxna@gmail.com>
2015-01-12 10:13:48 +0530

    Status: Use h-entry
    Bug: #36

--- content-status.php
+++ content-status.php
@@ -2,11 +2,16 @@
 
 <div class="entry-meta">
 	<footer>
-		<a href="<?php echo the_permalink() ?>"><?php the_date() ?> at <?php the_time() ?></a>
+		<a href="<?php echo the_permalink() ?>" class="u-url">
+			<time datetime="<?php the_time( 'Y-m-d H:i' ) ?>" class="dt-published">
+				<?php the_date() ?> at
+				<?php the_time() ?>
+			</time>
+		</a>
 	</footer>
 </div>
 
-<h1><?php the_title() ?></h1>
+<h1 class="p-name e-content"><?php the_title() ?></h1>
 
 
 </div>
