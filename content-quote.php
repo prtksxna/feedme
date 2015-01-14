86b12616e0f839fa86ddf90d0ad8c01d4f7c2856
Prateek Saxena <prtksxna@gmail.com>
2015-01-14 05:52:17 +0530

    Quote: Use h-entry
    Bug: #36

--- content-quote.php
+++ content-quote.php
@@ -2,11 +2,16 @@
 
 <div class="entry-meta">
 	<footer>
-		<a href="<?php echo the_permalink() ?>"><?php the_date() ?> at <?php the_time() ?></a>
+		<a href="<?php echo the_permalink() ?>">
+			<time datetime="<?php the_time( 'Y-m-d H:i' ) ?>" class="dt-published">
+				<?php the_date() ?> at
+				<?php the_time() ?>
+			</time>
+		</a>
 	</footer>
 </div>
 
-<div class="entry-content"><?php the_content() ?></div>
+<div class="entry-content e-content"><?php the_content() ?></div>
 <em><?php echo get_field( 'author' ) ?></em>
 
 
