f91b0005e2bdcadb90b8f4636e3ebb98547498e9
Prateek Saxena <prtksxna@gmail.com>
2017-07-18 17:55:44 +0530

    Use default content-post if no category is set

--- content-single.php
+++ content-single.php
@@ -5,40 +5,27 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'h-entry' ); ?>>
-	<?php if ( has_category('post') ) {
+	<?php
+	if ( has_category('post') ) {
 		get_template_part( 'content', 'post' );
-	} ?>
-
-	<?php if ( has_category('chat') ) {
+	} elseif ( has_category('chat') ) {
 		get_template_part( 'content', 'chat' );
-	} ?>
-
-	<?php if ( has_category('link') ) {
+	} elseif ( has_category('link') ) {
 		get_template_part( 'content', 'link' );
-	} ?>
-
-	<?php if ( has_category('photo') ) {
+	} elseif ( has_category('photo') ) {
 		get_template_part( 'content', 'photo' );
-	} ?>
-
-	<?php if ( has_category('status') ) {
+	} elseif ( has_category('status') ) {
 		get_template_part( 'content', 'status' );
-	} ?>
-
-	<?php if ( has_category('quote') ) {
+	} elseif ( has_category('quote') ) {
 		get_template_part( 'content', 'quote' );
-	} ?>
-
-	<?php if ( has_category('trip') ) {
+	} elseif ( has_category('trip') ) {
 		get_template_part( 'content', 'trip' );
-	} ?>
-
-	<?php if ( has_category('thing') ) {
+	} elseif ( has_category('thing') ) {
 		get_template_part( 'content', 'thing' );
-	} ?>
-
-
-	<?php if ( has_category('check-in') ) {
+	} elseif ( has_category('check-in') ) {
 		get_template_part( 'content', 'checkin' );
-	} ?>
+	} else {
+		get_template_part( 'content', 'post' );
+	}
+	?>
 </article><!-- #post-## -->
